6e62f560f8203d8787acaf1f3163ce80374c5f70
Stanisław Zakrzewski <sabrisv123@gmail.com>
2019-06-08 16:24:56 +0200

    all should work

--- src/main/java/model/FuzzySet.java
+++ src/main/java/model/FuzzySet.java
@@ -5,6 +5,7 @@ import model.membership_functions.MembershipFunction;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class FuzzySet {
@@ -12,26 +13,32 @@ public class FuzzySet {
     private LinkedList<FuzzyPair> fuzzySet;
     private MembershipFunction membershipFunction;
     private Map<Float, Float> valueMembership;
+    @Getter
+    private List<Float> values;
 
     public FuzzySet() {
         valueMembership = new HashMap<>();
         fuzzySet = new LinkedList<>();
+        values = new LinkedList<>();
     }
 
     public FuzzySet(MembershipFunction membershipFunction) {
         this.membershipFunction = membershipFunction;
         valueMembership = new HashMap<>();
         fuzzySet = new LinkedList<>();
+        values = new LinkedList<>();
     }
 
     public void addValue(float value) {
         fuzzySet.add(new FuzzyPair(value, membershipFunction.calculateMembership(value)));
         valueMembership.put(value, fuzzySet.getLast().getMembership());
+        values.add(value);
     }
 
     public void addValue(float value, float membership) {
         fuzzySet.add(new FuzzyPair(value, membership));
         valueMembership.put(value, membership);
+        values.add(value);
     }
 
     public float getCardinality() {
--- src/main/java/model/sentences/KSentence.java
+++ src/main/java/model/sentences/KSentence.java
@@ -3,41 +3,60 @@ package model.sentences;
 import lombok.Getter;
 import model.FuzzySet;
 import model.linguistic_quantifiers.LinguisticQuantifier;
+import model.linguistic_variables.CombinationLinguisticVariable;
 
 import java.util.List;
 
-public class KSentence {
-    private LinguisticQuantifier linguisticQuantifier;
+public class KSentence implements Sentence {
     @Getter
-    private List<FuzzySet> sFuzzySets;
+    private LinguisticQuantifier q;
     @Getter
-    private List<FuzzySet> wFuzzySets;
+    private CombinationLinguisticVariable s;
+    @Getter
+    private CombinationLinguisticVariable w;
 
-    public LinguisticQuantifier getQ() {
-        return linguisticQuantifier;
-    }
 
     public float getCombinedSWCardinality() {
-        return 0;
+        float value = 0;
+        for (Float f : s.getFuzzySet().getValues()) {
+            value += Math.min(s.getFuzzySet().getMembership(f), w.getFuzzySet().getMembership(f));
+        }
+        return value;
     }
 
     public float getCombinedWCardinality() {
-        return 0;
+        return s.getFuzzySet().getCardinality();
     }
 
     public float getWSCombinedSupp() {
-        return 0;
+        float value = 0;
+        for (Float f : s.getFuzzySet().getValues()) {
+            if (s.getFuzzySet().getMembership(f) > 0 && w.getFuzzySet().getMembership(f) > 0) value += 1;
+        }
+        return value;
     }
 
     public float getWCombinedSupp() {
-        return 0;
+        return w.getFuzzySet().getSupp();
     }
 
-    public float getQSize() {
-        return 0;
+    public List<FuzzySet> getWFuzzySets() {
+        return w.getFuzzySets();
     }
 
-    public int getQSupp() {
-        return 0;
+    public List<FuzzySet> getSFuzzySets() {
+        return s.getFuzzySets();
     }
+
+    @Override
+    public SentenceType getType() {
+        return SentenceType.K;
+    }
+
+    @Override
+    public String toString() {
+        return q.getName() + " lotów będących " + w.toString() + " jest " + s.toString();
+    }
+
+
 }
